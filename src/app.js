aeebdb5c3a82787833d1e4cc60a1a1d9ebf07349
Axel Alexis Montejo Lopez <alemontejo.lp@gmail.com>
2021-11-06 04:52:52 -0500

    Added CORS policy.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.static('public'))
 app.use('*', function(req, res) {
   let indexPath = path.join(__dirname, '..', 'public', 'index.html')
